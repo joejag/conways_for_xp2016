f52eb839948be15789afeba99770a723a46115f2
Joe Wright <joe@joejag.com>
2016-05-21 19:39:29 +0100

    OOSolver working

--- src/gol/solvers/OoSolver.java
+++ src/gol/solvers/OoSolver.java
@@ -1,5 +1,6 @@
 package gol.solvers;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,50 +10,59 @@ import static java.util.Arrays.asList;
 
 public class OoSolver {
 
-    private Map<int[], Cell> knownLife = new HashMap<>();
+    private Map<Point, Cell> knownLife = new HashMap<>();
+    private Map<Point, Cell> knownLifeAndSurroundingDeadCells = new HashMap<>();
 
     public static void main(String[] args) {
         OoSolver god = new OoSolver();
 
-        god.addLife(1, 1);
-        god.addLife(1, 2);
-        god.addLife(1, 3);
+        god.letThereBeLife(new Point(1, 1));
+        god.letThereBeLife(new Point(1, 2));
+        god.letThereBeLife(new Point(1, 3));
 
-        god.visit(new UniverseVisitor());
-
+        god.meetThyNeighbours();
         god.passJudgement();
 
         god.visit(new UniverseVisitor());
     }
 
-    private void passJudgement() {
-        for (int[] cords : knownLife.keySet()) {
-            int x = cords[0];
-            int y = cords[1];
+    // WORLD
 
-            List<int[]> neighbourCords = asList(
-                    new int[]{x - 1, y + 1}, new int[]{x, y + 1}, new int[]{x + 1, y + 1},
-                    new int[]{x - 1, y},     /* The middle */     new int[]{x + 1, y},
-                    new int[]{x - 1, y - 1}, new int[]{x, y - 1}, new int[]{x + 1, y - 1});
+    private void letThereBeLife(Point point) {
+        knownLife.put(point, new LiveCell(point));
+    }
 
-            for (int[] neighbourCord : neighbourCords) {
-                knownLife.putIfAbsent(neighbourCord, new DeadCell(neighbourCord[0], neighbourCord[1]));
-            }
+    private void meetThyNeighbours() {
+        knownLifeAndSurroundingDeadCells.putAll(knownLife);
 
-            List<Cell> neighbours = new ArrayList<>();
-            for (int[] neighbourCord : neighbourCords) {
-                neighbours.add(knownLife.get(neighbourCord));
-            }
+        for (Point cords : knownLife.keySet()) {
+            Cell liveCell = knownLifeAndSurroundingDeadCells.get(cords);
 
-            for (Cell cell : knownLife.values()) {
-                cell.emitEnergy(neighbours);
+            int x = cords.x;
+            int y = cords.y;
+
+            List<Point> neighbourCords = asList(
+                    new Point(x - 1, y + 1), new Point(x, y + 1), new Point(x + 1, y + 1),
+                    new Point(x - 1, y),     /* The middle */     new Point(x + 1, y),
+                    new Point(x - 1, y - 1), new Point(x, y - 1), new Point(x + 1, y - 1));
+
+            for (Point neighbourCord : neighbourCords) {
+                knownLifeAndSurroundingDeadCells.putIfAbsent(neighbourCord, new DeadCell(neighbourCord));
+
+                Cell neighbour = knownLifeAndSurroundingDeadCells.get(neighbourCord);
+                neighbour.addNeighbour(liveCell);
             }
         }
+    }
 
-        // count times life energy is present
-        // ask new world factory for new child
+    private void passJudgement() {
+        Map<Point, Cell> newWorld = new HashMap<>();
 
+        for (Cell cell : knownLifeAndSurroundingDeadCells.values()) {
+            cell.decideIfJoiningTheNewWorld(newWorld);
+        }
 
+        this.knownLife = newWorld;
     }
 
     private void visit(UniverseVisitor universeVisitor) {
@@ -61,9 +71,7 @@ public class OoSolver {
         }
     }
 
-    private void addLife(int x, int y) {
-        knownLife.put(new int[]{x, y}, new LiveCell(x, y));
-    }
+    // VISITOR
 
     private static class UniverseVisitor {
 
@@ -71,56 +79,64 @@ public class OoSolver {
             System.out.println("\nWorld is at...");
         }
 
-        public void visitLiveCell(int x, int y) {
-            System.out.println(x + " " + y);
+        public void visitLiveCell(Point point) {
+            System.out.println(point.x + " " + point.y);
         }
 
-        public void visitDeadCell(int x, int y) {
-            System.out.println(x + " " + y);
+        public void visitDeadCell(Point point) {
+            System.out.println(point.x + " " + point.y);
         }
     }
 
-    private abstract class Cell {
-        final int x;
-        final int y;
+    // CELLS
 
-        public Cell(int x, int y) {
-            this.x = x;
-            this.y = y;
+    private abstract class Cell {
+
+        protected final List<Cell> neighbours = new ArrayList<>();
+        protected final Point point;
+
+        public Cell(Point point) {
+            this.point = point;
         }
 
         public abstract void visit(UniverseVisitor universeVisitor);
 
-        public abstract void emitEnergy(List<Cell> neighbours);
+        public void addNeighbour(Cell cell) {
+            neighbours.add(cell);
+        }
+
+        public abstract void decideIfJoiningTheNewWorld(Map<Point, Cell> newWorld);
     }
 
     private class LiveCell extends Cell {
 
-        public LiveCell(int x, int y) {
-            super(x, y);
+        public LiveCell(Point point) {
+            super(point);
         }
 
         public void visit(UniverseVisitor universeVisitor) {
-            universeVisitor.visitLiveCell(x, y);
+            universeVisitor.visitLiveCell(point);
         }
 
-        public void emitEnergy(List<Cell> neighbours) {
-
+        public void decideIfJoiningTheNewWorld(Map<Point, Cell> newWorld) {
+            if (neighbours.size() == 2 || neighbours.size() == 3)
+                newWorld.put(point, new LiveCell(point));
         }
     }
 
     private class DeadCell extends Cell {
 
-        public DeadCell(int x, int y) {
-            super(x, y);
+        public DeadCell(Point point) {
+            super(point);
         }
 
         public void visit(UniverseVisitor universeVisitor) {
-            universeVisitor.visitDeadCell(x, y);
+            universeVisitor.visitDeadCell(point);
         }
 
-        public void emitEnergy(List<Cell> neighbours) {
-
+        public void decideIfJoiningTheNewWorld(Map<Point, Cell> newWorld) {
+            if (neighbours.size() == 3)
+                newWorld.put(point, new LiveCell(point));
         }
     }
 }
