44e295db143406fd6d979d9bcd2d2c3bdca5816f
Joe Wright <joe@joejag.com>
2016-05-21 14:59:41 +0100

    more tets

--- test/gol/AcceptanceTests.java
+++ test/gol/AcceptanceTests.java
@@ -26,6 +26,38 @@ public class AcceptanceTests {
         nextBlinkerWorld.add(new Point(2, 2));
         nextBlinkerWorld.add(new Point(2, 3));
         Assert.assertEquals(nextBlinkerWorld, nextGeneration);
+    }
 
+    @Test
+    public void testStillLife() {
+        Set<Point> stillLifeBlock = new HashSet<>();
+        stillLifeBlock.add(new Point(1, 2));
+        stillLifeBlock.add(new Point(2, 2));
+        stillLifeBlock.add(new Point(1, 3));
+        stillLifeBlock.add(new Point(2, 3));
+
+        Set<Point> nextGeneration = proceduralSolver.nextGeneration(stillLifeBlock);
+
+        Assert.assertEquals(stillLifeBlock, nextGeneration);
+    }
+
+    @Test
+    public void testSpaceShip() {
+        Set<Point> spaceShip = new HashSet<>();
+        spaceShip.add(new Point(1, 2));
+        spaceShip.add(new Point(2, 2));
+        spaceShip.add(new Point(3, 2));
+        spaceShip.add(new Point(3, 3));
+        spaceShip.add(new Point(2, 4));
+
+        Set<Point> nextGeneration = proceduralSolver.nextGeneration(spaceShip);
+
+        Set<Point> spaceShipNextStep = new HashSet<>();
+        spaceShipNextStep.add(new Point(2, 1));
+        spaceShipNextStep.add(new Point(2, 2));
+        spaceShipNextStep.add(new Point(3, 3));
+        spaceShipNextStep.add(new Point(1, 3));
+        spaceShipNextStep.add(new Point(3, 2));
+        Assert.assertEquals(spaceShipNextStep, nextGeneration);
     }
 }
