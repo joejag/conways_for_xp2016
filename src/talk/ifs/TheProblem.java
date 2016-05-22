17951b5302d929e68908674361b96ce51a256b43
Joe Wright <joe@joejag.com>
2016-05-22 23:41:08 +0100

    remove unused example code

---
+++ src/talk/ifs/TheProblem.java
@@ -0,0 +1,17 @@
+package talk.ifs;
+
+public class TheProblem {
+
+    public void theProblem() {
+
+        // some shared state
+
+        if(true) {
+            // something
+        } else {
+            // another something
+        }
+
+
+    }
+}
